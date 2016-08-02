e3009e73ac7a44ac3b7732426c972883c2e8b325
Rateev Ilya <dudeeer@gmail.com>
2016-08-02 22:30:18 +0300

    Created project

---
+++ src/main/java/ru/codebehind/steam/SteamAuth/App.java
@@ -0,0 +1,13 @@
+package ru.codebehind.steam.SteamAuth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
