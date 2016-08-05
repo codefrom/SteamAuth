c124c9fed2db88a51193dd82681e892bb61a4342
Rateev Ilya <dudeeer@gmail.com>
2016-08-05 10:00:09 +0400

    Starting with OkHttp for requests

--- src/main/java/ru/codebehind/steam/mobileauthentication/SteamWeb.java
+++ src/main/java/ru/codebehind/steam/mobileauthentication/SteamWeb.java
@@ -1,19 +1,22 @@
 package ru.codebehind.steam.mobileauthentication;
 
-import java.net.CookieStore;
-import java.net.URI;
-import java.util.Date;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.cookie.Cookie;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.JavaNetCookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class SteamWeb {
+	
+	public static String UserAgent = "Mozilla/5.0 (Linux; Android 4.4.4; Xperia S Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36";
+	
     /** 
      * Perform a mobile login request
      * @param url API url
@@ -23,29 +26,38 @@ public class SteamWeb {
      * @param headers 
      * @return response body
      */
-    public static String MobileLoginRequest(String url, String method, Map<String, String> data, CookieStore cookies, Map<String, String> headers) {
-        //return Request(url, method, data, cookies, headers, APIEndpoints.COMMUNITY_BASE + "/mobilelogin?oauth_client_id=DE45CD61&oauth_scope=read_profile%20write_profile%20read_client%20write_client");
+    public static String MobileLoginRequest(String url, String method, List<NameValuePair> data, CookieStore cookies, List<NameValuePair> headers) throws Throwable {
+        return Req(url, method, data, cookies, headers, APIEndpoints.COMMUNITY_BASE + "/mobilelogin?oauth_client_id=DE45CD61&oauth_scope=read_profile%20write_profile%20read_client%20write_client");
     }
     
-    public static String Req(String url, String method, List<NameValuePair> data, CookieStore cookies, List<NameValuePair> headers, String referer)
-    {
-    	Request req;
-    	URI uri = new URIBuilder(url).addParameters(data).build();
-    	switch (url) {
-			case "get":
-				req = Request.Get(uri);
-				break;
-			case "post":
-				req = Request.Post(uri);
-				break;
-		}
-
-    	for (NameValuePair header : headers) {
-    		req = req.addHeader(header.getName(), header.getValue());	
+    public static String InnerRequest(String url, String method, String dataString, /*CookieManager cookies, */List<Map.Entry<String, String>> headers, String referer) {
+    	CookieManager cookieManager = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
+    	
+    	OkHttpClient client = new OkHttpClient.Builder()
+    			.cookieJar(new JavaNetCookieJar(cookieManager))
+    			.build();
+    	
+    	HttpUrl.Builder uriBuilder = HttpUrl.parse(url).newBuilder();
+    	Request.Builder reqBuilder = new Request.Builder()
+    			.header("Accept", "text/javascript")
+    			.addHeader("Accept", "text/html")
+    			.addHeader("Accept", "application/xml")
+    			.addHeader("Accept", "text/xml")
+    			.addHeader("Accept", "*/*")
+    			.header("User-Agent", UserAgent);
+    	
+    	for (Map.Entry<String, String> header : headers) {
+    		reqBuilder.addHeader(header.getKey(), header.getValue());
 		}
     	
-    	Executor.newInstance().use(cookies).execute(req);
-
-        return Request(url, method, query, cookies, headers, referer);
+    	switch (method) {
+		case "get":
+			break;
+		case "post":
+			break;
+		default:
+			break;
+		}
+//    	Request req = new Request.Builder().;
     }
 }
